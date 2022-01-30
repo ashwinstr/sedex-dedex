17d62b6a850c62f689904ff209c2039a5c6ffe28
Kakashi <ashwinstr@gmail.com>
2022-01-30 17:20:55 +0530

    testing

--- jutsu/config.py
+++ jutsu/config.py
@@ -38,47 +38,25 @@ class Config:
 
     API_ID = int(os.environ.get("API_ID"))
     API_HASH = os.environ.get("API_HASH")
-    WORKERS = int(os.environ.get("WORKERS")) or os.cpu_count() + 4
     BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    HU_STRING_SESSION = os.environ.get("HU_STRING_SESSION")
     OWNER_ID = tuple(
         filter(lambda x: x, map(int, os.environ.get("OWNER_ID", "0").split()))
     )
     LOG_CHANNEL_ID = int(os.environ.get("LOG_CHANNEL_ID"))
-    AUTH_CHATS = (OWNER_ID[0], LOG_CHANNEL_ID) if OWNER_ID else (LOG_CHANNEL_ID,)
     DB_URI = os.environ.get("DATABASE_URL")
     LANG = os.environ.get("PREFERRED_LANGUAGE")
     DOWN_PATH = os.environ.get("DOWN_PATH")
-    CACHE_PATH = "userge/xcache"
-    CMD_TRIGGER = os.environ.get("CMD_TRIGGER")
-    SUDO_TRIGGER = os.environ.get("SUDO_TRIGGER")
+    CACHE_PATH = "jutsu/xcache"
+    CMD_TRIGGER = os.environ.get("CMD_TRIGGER", ";")
     FINISHED_PROGRESS_STR = os.environ.get("FINISHED_PROGRESS_STR")
     UNFINISHED_PROGRESS_STR = os.environ.get("UNFINISHED_PROGRESS_STR")
-    ALIVE_MEDIA = os.environ.get("ALIVE_MEDIA")
-    CUSTOM_PACK_NAME = os.environ.get("CUSTOM_PACK_NAME")
-    CUSTOM_PLUGINS_REPO = os.environ.get("CUSTOM_PLUGINS_REPO")
-    INSTA_ID = os.environ.get("INSTA_ID")
-    INSTA_PASS = os.environ.get("INSTA_PASS")
     UPSTREAM_REPO = os.environ.get("UPSTREAM_REPO")
     UPSTREAM_REMOTE = os.environ.get("UPSTREAM_REMOTE")
-    SPAM_WATCH_API = os.environ.get("SPAM_WATCH_API")
-    CURRENCY_API = os.environ.get("CURRENCY_API")
-    OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY")
-    OPEN_WEATHER_MAP = os.environ.get("OPEN_WEATHER_MAP")
-    REMOVE_BG_API_KEY = os.environ.get("REMOVE_BG_API_KEY")
-    WEATHER_DEFCITY = os.environ.get("WEATHER_DEFCITY")
     TZ_NUMBER = os.environ.get("TZ_NUMBER", 1)
-    G_DRIVE_CLIENT_ID = os.environ.get("G_DRIVE_CLIENT_ID")
-    G_DRIVE_CLIENT_SECRET = os.environ.get("G_DRIVE_CLIENT_SECRET")
-    G_DRIVE_PARENT_ID = os.environ.get("G_DRIVE_PARENT_ID")
-    G_DRIVE_INDEX_LINK = os.environ.get("G_DRIVE_INDEX_LINK")
     GOOGLE_CHROME_DRIVER = os.environ.get("GOOGLE_CHROME_DRIVER")
     GOOGLE_CHROME_BIN = os.environ.get("GOOGLE_CHROME_BIN")
-    GENIUS = os.environ.get("GENIUS")
     HEROKU_API_KEY = os.environ.get("HEROKU_API_KEY")
     HEROKU_APP_NAME = os.environ.get("HEROKU_APP_NAME")
-    G_DRIVE_IS_TD = os.environ.get("G_DRIVE_IS_TD") == "true"
-    LOAD_UNOFFICIAL_PLUGINS = os.environ.get("LOAD_UNOFFICIAL_PLUGINS") == "true"
     THUMB_PATH = DOWN_PATH + "thumb_image.jpg"
     TMP_PATH = "userge/plugins/temp/"
     MAX_MESSAGE_LENGTH = 4096
@@ -95,9 +73,6 @@ class Config:
     DISABLED_ALL = False
     DISABLED_CHATS: Set[int] = set()
     ALLOWED_COMMANDS: Set[str] = set()
-    ANTISPAM_SENTRY = False
-    SPAM_PROTECTION = False
-    RUN_DYNO_SAVER = False
     HEROKU_ENV = bool(int(os.environ.get("HEROKU_ENV", "0")))
     HEROKU_APP = (
         heroku3.from_key(HEROKU_API_KEY).apps()[HEROKU_APP_NAME]
@@ -107,23 +82,7 @@ class Config:
     STATUS = None
     BOT_FORWARDS = False
     BOT_MEDIA = os.environ.get("BOT_MEDIA")
-    SPOTIFY_CLIENT_ID = os.environ.get("SPOTIFY_CLIENT_ID")
-    SPOTIFY_CLIENT_SECRET = os.environ.get("SPOTIFY_CLIENT_SECRET")
-    SPOTIFY_MODE = False
-    IMGFLIP_ID = os.environ.get("IMGFLIP_ID")
-    IMGFLIP_PASS = os.environ.get("IMGFLIP_PASS")
-    ALLOW_NSFW = os.environ.get("ALLOW_NSFW", "False")
-    PM_LOG_GROUP_ID = int(os.environ.get("PM_LOG_GROUP_ID", 0))
-    PM_LOGGING = False
-    TAG_LOGGING = False
-    DEEP_AI = os.environ.get("DEEP_AI")
-    LASTFM_USERNAME = os.environ.get("LASTFM_USERNAME")
-    LASTFM_API_KEY = os.environ.get("LASTFM_API_KEY")
     TG_IDS = [777000, 1087968824, 454000]
-    INLINE_NOTES = False
-    BOT_ANTIFLOOD = False
-    NEW_ALIVE_MEDIA = None
-    ALIVE_MEDIA_TYPE = None
 
 
 def get_version() -> str:
